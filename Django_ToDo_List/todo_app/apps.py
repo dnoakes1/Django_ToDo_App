03300345186e9ca5bfb461f458d9e643cce91a79
dnoakes16@gmail.com <dnoakes16@gmail.com>
2022-03-21 01:18:27 -0400

    Commit App

---
+++ Django_ToDo_List/todo_app/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class TodoAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'todo_app'
+
+# todo_list/todo_app/urls.py
+
+urlpatterns = [
+]
